5c4fd9f23f2953ac6d51ce75fcebc62990883738
Brian Haas <brianjohnhaas@gmail.com>
2022-12-14 21:12:30 +0000

    add

--- Benchmarking_available_tools/methods/NF_VIF/test_run/runMe.sh
+++ Benchmarking_available_tools/methods/NF_VIF/test_run/runMe.sh
@@ -40,7 +40,6 @@ fi
 #	run ../WDL/ViFi.wdl \
 #	-i  inputs.json
 
-docker run -e USER='myself' --rm -it -v `pwd`:/data trinityctat/nf_vif:devel run_NF_VIF.sh
 
+docker run -e USER='myself' --rm -it -v `pwd`:/data trinityctat/nf_vif:devel bash /data/run_NF_VIF.sh
 
-# nextflow /usr/local/src/nf-VIF/main.nf   --reads "/data/*_R{1,2}.fq" --genome hg38 --bwt2_index /data/human_reference --fasta /data/ref_genome.fa --blatdb /data/GRCh38.genome.2bit --fasta_hpv /data/HPVs_db.fasta   --bwt2_index_hpv  /data/HPV_BWT2_index    --outdir /data/hpv16 --split_report --skip_trimming  --skip_fastqc   --skip_multiqc  --nb_geno 1 
