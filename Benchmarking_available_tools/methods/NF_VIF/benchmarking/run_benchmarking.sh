d380cb7aed007c3e72c2ead9cf2a24c2f9f792d7
Brian Haas <brianjohnhaas@gmail.com>
2022-12-15 20:39:59 +0000

    update

--- Benchmarking_available_tools/methods/NF_VIF/benchmarking/run_benchmarking.sh
+++ Benchmarking_available_tools/methods/NF_VIF/benchmarking/run_benchmarking.sh
@@ -2,29 +2,20 @@
 set -e
 
 
-# ./Reformating_vf_VIF.py \
-#     --output_directory /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/output/hpv_mapping/blat \
-#     --sample_tag subset.insertion_seqs.fa \
-#     --output /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/Benchmarking/vf_VIF_output.tsv
-
-./benchamrk_wrapper.py \
-    --output_directory /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/OUTPUT_Simulation \
+./scripts/benchmark_wrapper.py \
+    --output_directory `pwd`/data \
     --viruses HPV45,HPV39,HPV33,HPV35,HPV16,HPV31,HPV18 \
-    --output /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/OUTPUT_Simulation/combined_vf_VIF_output.tsv
+    --output `pwd`/data/combined_vf_VIF_output.tsv
     
-# ./Reformating_vf_VIF.py \
-#     --output_directory /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/OUTPUT_Simulation \
-#     --sample_tag subset.insertion_seqs.fa \
-#     --output /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/Benchmarking/combined_vf_VIF_output.tsv
+
+./scripts/Benchmarking.py \
+        --truth_insertions `pwd`/insertion_truth_set.tsv \
+        --predicted_insertions `pwd`/data//combined_vf_VIF_output.tsv \
+        --output `pwd`/benchmarking_out
+
+
+./scripts/analyze_VIF_TP_FP_FNs.Rscript \
+    --dat `pwd`/benchmarking_out/Benchmarking_Output.txt
 
 
 
-/home/mbrown/GitHub/VirusInsertionBenchmarking_old/VirusInsertionBenchmarking/util/Other_Pipelines/Benchmarking.py \
-        --truth_insertions /home/mbrown/CTAT_VIF/Simulation/CreateSimulations_50/insertion_truth_set.tsv \
-        --predicted_insertions /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/OUTPUT_Simulation/combined_vf_VIF_output.tsv \
-        --output /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/Benchmarking
-
-
-
-/home/mbrown/GitHub/VirusInsertionBenchmarking_old/VirusInsertionBenchmarking/util/analyze_VIF_TP_FP_FNs.Rscript \
-    --dat /home/mbrown/CTAT_VIF/Other_Pipelines/NF_VIF/Benchmarking/Benchmarking_Output.txt
