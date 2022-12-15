08616f57c646f1de75107a59087c5513a102dd48
Brian Haas <brianjohnhaas@gmail.com>
2022-12-15 20:18:01 +0000

    update

--- Benchmarking_available_tools/methods/ViFi/benchmarking/reformat_Benchmark_vif.sh
+++ Benchmarking_available_tools/methods/ViFi/benchmarking/reformat_Benchmark_vif.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 
 # Reformat
-/home/mbrown/GitHub/VirusInsertionBenchmarking/util/Other_Pipelines/ViFi/reformatViFi.py \
-        --inputs ./Test/*.clusters.txt \
+./scripts/reformatViFi.py \
+        --inputs ./data/Test.clusters.txt \
         --sampleID subset.insertion_seqs.fa \
-        --output `pwd`/benchmarking
+        --output `pwd`/benchmarking_out
 
 
-
-# Benchmark
-#./../Benchmarking.py
-#/home/mbrown/GitHub/VirusInsertionBenchmarking/util/Other_Pipelines/Benchmarking.py \
-#	--truth_insertions /home/mbrown/CTAT_VIF/Simulation/CreateSimulations/truthset.tsv \
-#        --predicted_insertions /home/mbrown/CTAT_VIF/Other_Pipelines/ViFi/ViFi_Output_reformated.txt \
-#        --output /home/mbrown/CTAT_VIF/Other_Pipelines/ViFi
-
-# /home/mbrown/GitHub/VirusInsertionBenchmarking/util/Other_Pipelines/Benchmarking.py \
-/home/mbrown/GitHub/VirusInsertionBenchmarking_old/VirusInsertionBenchmarking/util/Other_Pipelines/Benchmarking.py \
-        --truth_insertions /home/mbrown/CTAT_VIF/Simulation/CreateSimulations_50/insertion_truth_set.tsv \
-        --predicted_insertions `pwd`/benchmarking/ViFi_Output_reformated.txt \
-        --output `pwd`/benchmarking
+./scripts/Benchmarking.py \
+        --truth_insertions `pwd`/insertion_truth_set.tsv \
+        --predicted_insertions `pwd`/benchmarking_out/ViFi_Output_reformated.txt \
+        --output `pwd`/benchmarking_out
 
 # STATS
-# /home/mbrown/GitHub/VirusInsertionBenchmarking/util/analyze_VIF_TP_FP_FNs.Rscript \
-/home/mbrown/GitHub/VirusInsertionBenchmarking_old/VirusInsertionBenchmarking/util/analyze_VIF_TP_FP_FNs.Rscript \
-        --dat `pwd`/benchmarking/Benchmarking_Output.txt
+./scripts/analyze_VIF_TP_FP_FNs.Rscript \
+        --dat `pwd`/benchmarking_out/Benchmarking_Output.txt
+
+
+
