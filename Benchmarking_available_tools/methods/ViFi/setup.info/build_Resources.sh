11e7041095f261165cf8eb0469ceb17e78940ab3
Brian Haas <brianjohnhaas@gmail.com>
2022-12-20 21:01:00 +0000

    add

---
+++ Benchmarking_available_tools/methods/ViFi/setup.info/build_Resources.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+
+
+
+
+
+
+
+
+
+/home/mbrown/GitHub/Maxwell_ViFi/ViFi/ViFi//scripts/build_references.sh \
+    /home/mbrown/CTAT_VIF/Other_Pipelines/References/HPV_only.fasta \
+    /home/mbrown/CTAT_VIF/Other_Pipelines/References/HPV_HMM \
+    hpv \
+    /home/mbrown/CTAT_VIF/Other_Pipelines/ViFi/ViFi/scripts
