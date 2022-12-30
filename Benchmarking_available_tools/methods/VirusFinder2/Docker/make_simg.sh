b931633f8a96f6d71aecbd717233ba771403a6bb
Brian Haas <brianjohnhaas@gmail.com>
2022-12-30 19:13:28 +0000

    update

--- Benchmarking_available_tools/methods/VirusFinder2/Docker/make_simg.sh
+++ Benchmarking_available_tools/methods/VirusFinder2/Docker/make_simg.sh
@@ -5,3 +5,5 @@ singularity build  virusfinder2.simg  docker://trinityctat/virusfinder2:devel
 
 singularity exec -e -C virusfinder2.simg /usr/local/src/VF2Verse_runner.py --help
 
+# copy to:
+# gs://trinityctatvirusinsertionfinder/SINGULARITY/other_pipelines/virusfinder2.simg
