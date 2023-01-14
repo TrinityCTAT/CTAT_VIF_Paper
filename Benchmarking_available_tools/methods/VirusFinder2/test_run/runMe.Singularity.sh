15d32545919bdeba883597f74fc4e467d471e6f9
Brian Haas <brianjohnhaas@gmail.com>
2023-01-14 14:51:36 +0000

    run test w/ hpv33 for VF2verse

--- Benchmarking_available_tools/methods/VirusFinder2/test_run/runMe.Singularity.sh
+++ Benchmarking_available_tools/methods/VirusFinder2/test_run/runMe.Singularity.sh
@@ -7,10 +7,13 @@ if [ ! -e virusfinder2.simg ]; then
     gsutil cp gs://trinityctatvirusinsertionfinder/SINGULARITY/other_pipelines/virusfinder2.simg .
 fi
 
-if [ ! -e subset_insertion_seqs_QSadjust_50ins_7sam_1.fq ]; then
+if [ ! -e rep1.HPV33.insertion_seqs_1.fq.gz ]; then
 
-    gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/subset_insertion_seqs_QSadjust_50ins_7sam_1.fq .
-    gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/subset_insertion_seqs_QSadjust_50ins_7sam_2.fq .
+    gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/rep1.HPV33.insertion_seqs_1.fq.gz .
+    gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/rep1.HPV33.insertion_seqs_2.fq.gz .
+    
+    #gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/subset_insertion_seqs_QSadjust_50ins_7sam_1.fq .
+    #gsutil cp gs://trinityctatvirusinsertionfinder/OTHER_PIPELINES/VirusFinder2/subset_insertion_seqs_QSadjust_50ins_7sam_2.fq .
     
 fi
 
@@ -39,6 +42,6 @@ fi
 
 rootdir=`pwd`
 
-cd singularity_outdir && cp ../*.fq . && singularity exec -e -B $rootdir  ../virusfinder2.simg /usr/local/src/VF2Verse_runner.py  --human_resource_dir `pwd`/../human_reference --virus_resource_dir `pwd`/../virus_reference --target_virus HPV16 --left_fq subset_insertion_seqs_QSadjust_50ins_7sam_1.fq --right_fq subset_insertion_seqs_QSadjust_50ins_7sam_2.fq --sample_id testme
+cd singularity_outdir && cp ../*.fq.gz . && singularity exec -e -B $rootdir  ../virusfinder2.simg /usr/local/src/VF2Verse_runner.py  --human_resource_dir `pwd`/../human_reference --virus_resource_dir `pwd`/../virus_reference --target_virus HPV33 --left_fq rep1.HPV33.insertion_seqs_1.fq.gz --right_fq rep1.HPV33.insertion_seqs_2.fq.gz --sample_id testme
 
 
